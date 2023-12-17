86f2d1723860e62f1fae82ce55a351c22e44c005
chengsi <chengsi.zhang@se.com>
2023-12-17 23:36:56 +0800

    fff

---
+++ i18n/run/index.ts
@@ -0,0 +1,26 @@
+/* eslint-disable no-bitwise */
+/* eslint-disable no-underscore-dangle */
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import { ReplaceBundle } from '../ReplaceBundle';
+
+const testDir = path.join(process.cwd(), 'i18n/test');
+
+const dist = path.join(testDir, 'dist');
+
+fs.ensureDirSync(dist);
+const replaceBundle = new ReplaceBundle({
+  outputDir: 'c:\\work\\eh-ui\\assets',
+  fileReplaceOverwirte: true,
+  srcTargets: [
+    'C:\\work\\eh-ui\\components',
+    'C:\\work\\eh-ui\\pages',
+    'C:\\work\\eh-ui\\components',
+    'C:\\work\\eh-ui\\middleware',
+    'C:\\work\\eh-ui\\utils',
+    // 'C:\\work\\eh-ui\\modules',
+  ],
+  ouputImportPath: 'Ass',
+  // debug: true,
+});
+replaceBundle.bundleReplace();
